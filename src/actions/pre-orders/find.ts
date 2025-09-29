c99d3d6dc8f418670e9e8ef289e5a7f5e9f641fa
Jackson Samuel Xavier dos Santos <jackson144@gmail.com>
2025-09-28 23:34:54 -0300

    fix find

--- src/actions/pre-orders/find.ts
+++ src/actions/pre-orders/find.ts
@@ -1,16 +1,16 @@
 "use server";
 
-import { PreOrderFindResponse } from "@/interfaces";
+import { PreOrderFindInitialResponse } from "@/interfaces";
 import { redirect } from "next/navigation";
 
-export const findPreOrder = async (id: string): Promise<PreOrderFindResponse | null> => {
+export const findPreOrder = async (id: string): Promise<PreOrderFindInitialResponse | null> => {
   try {
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pre-sale/${id}`, {
       cache: "no-store", // sempre buscar fresh
     });
 
-    const data: PreOrderFindResponse | null = await res.json();
+    const data: PreOrderFindInitialResponse | null = await res.json();
     //console.info('findPreOrder:', data);
     return data
   } catch (error) {
